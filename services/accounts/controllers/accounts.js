b32872b0f640b3b20a865cc02363eb05d5ad0949
Alexander Rozhok <mizzdev@gmail.com>
2017-07-27 10:36:04 +0300

    Added BIG BROTHER STYLE logging

--- services/accounts/controllers/accounts.js
+++ services/accounts/controllers/accounts.js
@@ -1,10 +1,16 @@
 'use strict';
 
+const log4js = require('log4js');
 const Account = require('../models/account');
+
+const logger = log4js.getLogger('accounts');
+
+logger.level = 'debug';
 
 exports.create = function(req, res) {
   const account = new Account(req.body);
   account.save()
+    .tap((account) => logger.info(`[ID ${account.id}]`, 'registered', account.nickname))
     .then((account) => res.json(account))
     .catch((err) => res.status(500).send(err.message));
 };
@@ -28,11 +34,14 @@ exports.update = function(req, res) {
         return res.status(404).send('Account Not Found');
       }
 
+      logger.info(`[ID ${account.id}]`, 'before update:', account);
+
       Object.keys(req.body).forEach((key) => {
         account[key] = req.body[key];
       });
 
       return account.save()
+        .tap((account) => logger.info(`[ID ${account.id}]`, 'after update:', account))
         .then((account) => res.json(account));
     })
     .catch((err) => res.status(500).send(err.message));
