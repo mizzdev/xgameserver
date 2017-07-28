42334cc7313fb29f4c9b70466954fe8cee1f33db
Alexander Rozhok <mizzdev@gmail.com>
2017-07-28 10:54:46 +0300

    Implemented field whitelisting for account update query

--- services/accounts/index.js
+++ services/accounts/index.js
@@ -4,10 +4,12 @@ const express = require('express');
 const router = express.Router();
 
 const accountsController = require('./controllers/accounts');
+
 const checkAuth = require('./middlewares/checkAuth');
+const accountFieldWhitelist = require('./middlewares/accountFieldWhitelist');
 
 router.get('/:id', checkAuth, accountsController.read);
-router.put('/:id', checkAuth, accountsController.update);
+router.put('/:id', checkAuth, accountFieldWhitelist, accountsController.update);
 router.post('/', accountsController.create);
 
 exports.name = 'accounts';
---
+++ services/accounts/middlewares/accountFieldWhitelist.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const config = require('../config.json');
+
+module.exports = function(req, res, next) {
+  Object.keys(req.body).forEach((key) => {
+    if (!config['ACCOUNT_FIELD_WHITELIST'].some((field) => (field === key))) {
+      delete req.body[key];
+    }
+  });
+
+  next();
+};
