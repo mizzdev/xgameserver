7115e8d622b460f16fdbf9c455b61b8295034658
Alexander Rozhok <mizzdev@gmail.com>
2017-08-07 22:09:56 +0300

    Added apns support to the notification service

--- services/notifications/api.js
+++ services/notifications/api.js
@@ -1,7 +1,10 @@
 'use strict';
 
 const Promise = require('bluebird');
+const should = require('should');
 const Notification = require('./models/notification');
+
+const serviceRegistry = require('../registry.js');
 
 exports.getInbox = function(accountId) {
   return Notification.find({ accountId });
@@ -12,5 +15,22 @@ exports.send = function(data) {
     .then(() => {
       const notification = new Notification(data);
       return notification.save();
+    })
+    .then(() => {
+      const accountsService = serviceRegistry.getService('accounts');
+      return accountsService.getAccountById(data.accountId);
+    })
+    .then((account) => {
+      should.exist(account, 'Recipient account missing');
+      return account;
+    })
+    .tap((account) => {
+      const apnsService = serviceRegistry.getService('apns');
+
+      return Promise.map(account.deviceTokensIOS, (deviceToken) => {
+        return apnsService.send(deviceToken, data.title);
+      }).catch(() => {
+        throw new Error('APNS Delivery Failed');
+      });
     });
 };
