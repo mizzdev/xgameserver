9b55c200bf3812deecdecc13fda1d68f98651ba8
Alexander Rozhok <mizzdev@gmail.com>
2017-08-20 05:23:49 +0300

    Added general semaphorize method to the corresponding plugin

--- services/notifications/models/semaphorize.js
+++ services/notifications/models/semaphorize.js
@@ -1,12 +1,14 @@
 'use strict';
 
+const Promise = require('bluebird');
 const should = require('should');
 
 module.exports = function semaphorizePlugin(schema, options) {
-  let timeout = 0;
+  let timeout = 0, retryInterval = 1000;
 
-  if (options && options.timeout) {
-    timeout = options.timeout;
+  if (options) {
+    timeout = options.timeout || timeout;
+    retryInterval = options.retryInterval || retryInterval;
   }
 
   schema.add({ _lock: Date });
@@ -18,14 +20,14 @@ module.exports = function semaphorizePlugin(schema, options) {
       { $currentDate: { _now: true } },
       { new: true }
     )
-      .then((entity) => {
-        should.exist(entity, 'Entity not found');
+      .then((doc) => {
+        should.exist(doc, 'Document not found');
 
         let query = {
           id: id,
           $or: [
             { _lock: { $exists: false } },
-            { _lock: { $lt: new Date((timeout) ? (entity._now - timeout) : 0) } }
+            { _lock: { $lt: new Date((timeout) ? (doc._now - timeout) : 0) } }
           ]
         };
 
@@ -40,5 +42,34 @@ module.exports = function semaphorizePlugin(schema, options) {
   schema.statics.unlock = function(id) {
     return this.update({ id }, { $unset: { _lock: '' } })
       .then((result) => Boolean(result.n));
+  };
+
+  schema.methods.semaphorize = function(methodName, methodArguments, unlockOnSuccess) {
+    unlockOnSuccess = (typeof unlockOnSuccess === 'undefined') ? true : unlockOnSuccess;
+
+    return Promise.resolve()
+      .then(() => {
+        should(methodName).be.a.String();
+        should(methodArguments).be.an.Array();
+        should(this[methodName]).have.a.type('function', 'This document does not have such method');
+      })
+      .then(() => this.constructor.lock(this.id))
+      .then((lockSuccess) => {
+        if (!lockSuccess) {
+          return Promise.delay(retryInterval)
+            .then(this.semaphorize.bind(this, methodName, methodArguments, unlockOnSuccess));
+        }
+
+        const operation = this.constructor.findOne({ id: this.id })
+          .then((doc) => doc[methodName].apply(this, methodArguments));
+
+        if (unlockOnSuccess) {
+          return operation.finally(() => this.constructor.unlock(this.id));
+        } else {
+          return operation.catch((err) => {
+            return this.constructor.unlock(this.id).then(() => { throw err; });
+          });
+        }
+      });
   };
 };
