d6d1e0bec789c58fdab0a29d042132ccb6497c45
Alexander Rozhok <mizzdev@gmail.com>
2017-08-26 05:29:15 +0300

    Added order expiration periodic check

--- services/market/index.js
+++ services/market/index.js
@@ -3,6 +3,11 @@
 const express = require('express');
 const router = express.Router();
 
+const config = require('./config.json');
+const env = require('../../env');
+
+const orderExpirationProcedure = require('./procedures/order-expiration');
+
 const ordersController = require('./controllers/orders');
 
 const checkAuth = require('./middlewares/check-auth');
@@ -17,3 +22,8 @@ router.delete('/orders/:orderId', ordersController.fulfill);
 exports.name = 'market';
 exports.router = router;
 exports.serviceInterface = require('./api');
+
+const checkPeriod = env('MARKET_ORDER_CHECK_PERIOD_MS') || config['MARKET_ORDER_CHECK_PERIOD_MS'];
+exports.init = function() {
+  setInterval(orderExpirationProcedure, checkPeriod);
+};
